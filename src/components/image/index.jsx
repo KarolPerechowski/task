8b4077431aea07922b3f8ed9b2f9b1a31bb4b347
karol <karol.perechowski@gmail.com>
2024-11-18 21:25:39 +0100

    Task changes

--- src/components/image/index.jsx
+++ src/components/image/index.jsx
@@ -1,11 +1,70 @@
-const Image = ({ src, alt, className, width = 'auto', height = 'auto' }) => {
+import { useState, useEffect } from "react";
+
+const Image = ({ src, alt, className, width = 'auto', height = 'auto', srcSet, link, onHover, imageStyle, skeleton = false }) => {
+
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [hasError, setHasError] = useState(false);
 
   const handleImageError = (e) => {
     e.target.alt = 'Image failed to load';
+    setHasError(true);
   };
 
-  return <img src={src} alt={alt} className={className} width={width} height={height} onError={handleImageError} />;
+  const handleImageLoad = () => {
+    setIsLoaded(true);
+  };
 
+  const checkIfInViewport = () => {
+    const rect = imageRef.current?.getBoundingClientRect();
+    if (rect && rect.top <= window.innerHeight) {
+      setIsInViewport(true);
+    }
+  };
+
+  useEffect(() => {
+    if (skeleton && !isLoaded) {
+      checkIfInViewport(); 
+      window.addEventListener('scroll', checkIfInViewport);
+
+      return () => window.removeEventListener('scroll', checkIfInViewport); 
+    }
+  }, [isLoaded, skeleton]);
+
+  return (
+  <div>
+    {!isLoaded && skeleton && !hasError && (
+      <div className="skeleton-loader" style={{ width, height }} />
+    )}
+    
+    {link ? 
+    <a href={link}>
+      <img
+        src={src}
+        srcSet={srcSet}
+        alt={alt || "lorem ipsum"}
+        className={className} 
+        width={width}
+        height={height}
+        loading="lazy"
+        onMouseEnter={onHover}
+        style={imageStyle}
+        onLoad={handleImageLoad}
+        onError={handleImageError} />
+    </a> :
+      <img
+        src={src}
+        srcSet={srcSet}
+        alt={alt || "lorem ipsum"}
+        width={width}
+        height={height}
+        className={className} 
+        loading="lazy"
+        onMouseEnter={onHover}
+        style={imageStyle}
+        onLoad={handleImageLoad}
+        onError={handleImageError} />}
+  </div>
+  )
 };
 
 export default Image;
--- src/sections/contact/index.jsx
+++ src/sections/contact/index.jsx
@@ -1,10 +1,36 @@
+import { useState } from 'react';
+import FormInput from '../../components/formInput';
+import contactModel from '../../models/Contact.model';
 import './index.css';
 
 const Contact = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [service, setService] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(firstName, lastName, phone, service)
+  }
 
   return (
     <div className="contact">
-      :)
+      <div className="container">
+        <div className="content">
+          <h2>{contactModel.heading}</h2>
+          <p>{contactModel.text}</p>
+        </div>
+        <div className='contactForm'>
+            <div className='nameInputs'>
+              <FormInput onChange= {(e) => setFirstName(e.target.value)} type="text" placeholder='First Name'></FormInput>
+              <FormInput onChange= {(e) => setLastName(e.target.value)} type="text" placeholder='Last Name'></FormInput>
+            </div>
+            <FormInput onChange= {(e) => setPhone(e.target.value)} type="tel" style={"full"} placeholder='Number Phone'></FormInput>
+            <FormInput onChange= {(e) => setService(e.target.value)} type="text" style={"full"} placeholder='What Service are you interested in?'></FormInput>
+          <a href="#" className="btn" type='submit' onClick={handleSubmit}>SUBMIT NOW</a>
+        </div>
+      </div>
     </div>
   )
 };
